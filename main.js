ddab830d2d159b701b34ff8e979e13375e441b20
C1ickb8 <dalvez.owens@protonmail.com>
2022-09-19 16:27:43 -0400

    update color calculator

---
+++ main.js
@@ -0,0 +1,48 @@
+const prompt = require('prompt-sync') ({sigint: true});
+
+let choice = String(prompt("Would you like to combine or deconstruct?: "))
+    console.log("You've chosen " + choice);
+
+let red = "red"
+let blue = "blue"
+let yellow = "yellow"
+let purple = "purple"
+let orange = "orange"
+let green = "green"
+
+if(choice === "combine"){       
+color1 = String(prompt("Select red or blue: "))
+color2 = String(prompt("Select blue or yellow: ")) 
+    if(color1 === red && color2 === blue){
+    console.log("The combined color is purple")
+        } if(color1 === red && color2 === yellow){
+        console.log("The combined color is orange")
+            } if(color1 === blue && color2 === yellow){
+            console.log("The combined color is green")
+            } 
+    } 
+
+if(choice === "deconstruct"){
+color3 = String(prompt("Enter purple, orange, or green: "));
+    if(color3 === purple){
+    console.log("The deconstructed colors are red and blue")
+        } if(color3 === orange){
+        console.log("The deconstructed colors are red and yellow")
+            } if(color3 === green){
+            console.log("The deconstructed colors are blue and yellow")
+            }    
+} else{
+    console.log("error")
+}
+
+
+
+
+
+
+
+
+
+
+
+
